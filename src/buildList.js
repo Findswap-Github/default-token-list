754e70b3c6df416bfafa695b66e10379b39f5aeb
shr1ftyy <syeamtechdemon@gmail.com>
2021-12-09 01:18:13 +1030

    findora default

--- src/buildList.js
+++ src/buildList.js
@@ -4,11 +4,12 @@ const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
 const kovan = require("./tokens/kovan.json");
+const findora_forge = require("./tokens/findora_forge.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Uniswap Default List",
+    name: "Findswap List",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -17,8 +18,8 @@ module.exports = function buildList() {
     },
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby]
+    keywords: ["findswap", "default"],
+    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...findora_forge]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
